e6f4cb0783521a1485222c9333e99d07e0702e8c
孙全超 <sunquanchao@zhongchebaolian.com>
2017-06-29 16:35:25 +0800

    基本可用版本

--- cloud-simple-service/src/main/java/cloud/simple/service/HealthIndicatorSpringBootApplication.java
+++ cloud-simple-service/src/main/java/cloud/simple/service/HealthIndicatorSpringBootApplication.java
@@ -17,19 +17,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 //http://blog.csdn.net/clementad/article/details/52045495
 //@SpringBootApplication  
 //@EnableScheduling  
-public class HealthIndicatorSpringBootApplication implements HealthIndicator{  
-    private static Logger logger = LoggerFactory.getLogger(HealthIndicatorSpringBootApplication.class);  
-      
-    public static void main(String[] args) {  
-        SpringApplication.run(HealthIndicatorSpringBootApplication.class, args);  
-        logger.info("My Spring Boot Application Started");  
-    }  
-  
-    /** 
-     * 在/health接口调用的时候，返回多一个属性："mySpringBootApplication":{"status":"UP","hello":"world"} 
-     */  
-    @Override  
-    public Health health() {  
-        return Health.up().withDetail("hello", "world").build();  
-    }  
-}  
+//public class HealthIndicatorSpringBootApplication implements HealthIndicator{  
+//    private static Logger logger = LoggerFactory.getLogger(HealthIndicatorSpringBootApplication.class);  
+//      
+//    public static void main(String[] args) {  
+//        SpringApplication.run(HealthIndicatorSpringBootApplication.class, args);  
+//        logger.info("My Spring Boot Application Started");  
+//    }  
+//  
+//    /** 
+//     * 在/health接口调用的时候，返回多一个属性："mySpringBootApplication":{"status":"UP","hello":"world"} 
+//     */  
+//    @Override  
+//    public Health health() {  
+//        return Health.up().withDetail("hello", "world").build();  
+//    }  
+//}  
