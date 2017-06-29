398a206060bd26ac084077c379f358e24fc6d592
孙全超 <sunquanchao@zhongchebaolian.com>
2017-06-29 15:37:19 +0800

    init db.sql

--- cloud-simple-service/src/main/resources/db.sql
+++ cloud-simple-service/src/main/resources/db.sql
@@ -3,3 +3,9 @@ CREATE TABLE `user` (
   `username` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `user` VALUES ('1', 'sqc11');
+INSERT INTO `user` VALUES ('2', 'sqc22');
+INSERT INTO `user` VALUES ('3', 'sqc33');
+INSERT INTO `user` VALUES ('4', 'sqc44');
+INSERT INTO `user` VALUES ('5', 'fuck55');
+INSERT INTO `user` VALUES ('6', 'fuck66');
